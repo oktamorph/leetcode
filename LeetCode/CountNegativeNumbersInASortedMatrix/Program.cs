83e02719faf816bdb0812b8b36934c30debab6d9
oktamorph <oktamorph@gmail.com>
2023-12-12 22:13:40 +0100

    add the project to resolve a 'Count Negative Numbers in a Sorted Matrix' problem

---
+++ LeetCode/CountNegativeNumbersInASortedMatrix/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution
+{
+    public int CountNegatives(int[][] grid)
+    {
+        int number = 0;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] < 0)
+                    number++;
+            }
+        }
+
+        return number;
+    }
+}
