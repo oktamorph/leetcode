5340139580e91d3c00ec8488d3523f772763ff84
oktamorph <oktamorph@gmail.com>
2023-12-12 22:43:17 +0100

    add the project to resolve the 'Binary Search' problem

---
+++ LeetCode/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int low = 0;
+        int high = nums.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int guess = nums[mid];
+
+            if (guess == target)
+                return mid;
+            if (guess > target)
+                high = mid - 1;
+            if (guess < target)
+                low = mid + 1;
+        }
+
+        return -1;
+    }
+}
--- LeetCode/BinarySearch/CountNegativeNumbersInASortedMatrix/Program.cs
+++ LeetCode/BinarySearch/CountNegativeNumbersInASortedMatrix/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-public class Solution
+﻿public class Solution
 {
     public int CountNegatives(int[][] grid)
     {
