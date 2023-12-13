5a2f8d9eb4ebf1dd2529e47050b4851cb5a52de4
oktamorph <oktamorph@gmail.com>
2023-12-13 13:58:38 +0100

    add the project for the 'Valid Perfect Square' problem

---
+++ LeetCode/BinarySearch/ValidPerfectSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool IsPerfectSquare(int num)
+    {
+        long min = 1;
+        long max = num;
+
+        while (min <= max)
+        {
+            long middle = min + (max - min) / 2;
+            long square = middle * middle;
+
+            if (square > num)
+                max = middle - 1;
+            else if (square < num)
+                min = middle + 1;
+            else
+                return true;
+        }
+
+        return false;
+    }
+}
