e72285d4b7a0ff8b6b61e4f005c0f895d5d0e9eb
oktamorph <oktamorph@gmail.com>
2023-12-13 22:41:47 +0100

    add the project for the 'How many numbers are smaller than the current number' problem

---
+++ LeetCode/BinarySearch/HowManyNumbersAreSmallerThanTheCurrentNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        int count = 0;
+
+        while (count < nums.Length)
+        {
+            int innerCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == i)
+                    continue;
+                if (nums[count] > nums[i])
+                    innerCount++;
+            }
+
+            result[count] = innerCount;
+            count++;
+        }
+
+        return result;
+    }
+}
