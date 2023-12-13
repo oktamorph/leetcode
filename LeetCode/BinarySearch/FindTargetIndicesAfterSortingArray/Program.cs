db2c20e2e005b7d3f9db5dabe1bd76a5cbcc955c
oktamorph <oktamorph@gmail.com>
2023-12-13 11:35:42 +0100

    add the project for the 'Find Target Indices After Sorting Array' problem

---
+++ LeetCode/BinarySearch/FindTargetIndicesAfterSortingArray/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public IList<int> TargetIndices(int[] nums, int target)
+    {
+        Array.Sort(nums);
+
+        var result = new List<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+                result.Add(i);
+        }
+
+        if (result.Count > 0)
+            return result;
+
+        return new List<int>();
+    }
+}
