39b7912dec614c145181cff030a657b1f710ba9b
oktamorph <oktamorph@gmail.com>
2023-12-15 22:17:54 +0100

    add the project for the "Power of two" problem

---
+++ LeetCode/PowerOfTwo/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n == 1) return true;
+        if (n <= 0 || n % 2 != 0) return false;
+
+        return IsPowerOfTwo(n / 2);
+    }
+}
