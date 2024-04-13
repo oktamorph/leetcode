96efd6a63c6997a1a13f098ebf74a65058b0e168
oktamorph <oktamorph@gmail.com>
2024-04-13 10:06:28 +0200

    add new "MissingNumber" project

---
+++ LeetCode/MissingNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿public int MissingNumber(int[] nums)
+{
+    if (nums.Length < 1 || nums.Length > 10000)
+        return 0;
+
+    for (var i = 0; i < nums.Length + 1; i++)
+    {
+        if (!nums.Contains(i))
+            return i;
+    }
+
+    return 0;
+}
