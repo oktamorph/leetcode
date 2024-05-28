157134ebcdaafe0441d8a154ff656e32538e797a
oktamorph <oktamorph@gmail.com>
2024-05-28 23:14:23 +0200

    implement the solution for the "Find a corresponding node of a binary tree in a clone of that tree" problem

---
+++ LeetCode/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode tree = new TreeNode(8) 
+                { 
+                    left = null, 
+                    right = new TreeNode(6)
+                    {
+                        left = null,
+                        right = new TreeNode(5)
+                        {
+                            left = null,
+                            right = new TreeNode(4)
+                            {
+                                left = null,
+                                right = new TreeNode(3)
+                                {
+                                    left = null,
+                                    right = new TreeNode(2)
+                                    {
+                                        left = null,
+                                        right = new TreeNode(1)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+
+            TreeNode target = new TreeNode(4);
+
+            var result = GetTargetCopy(tree, tree, target);
+        }
+
+        public static TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+        {
+            if (original is null) return original;
+            if (original == target) return cloned;
+            return GetTargetCopy(original.left, cloned.left, target) ?? GetTargetCopy(original.right, cloned.right, target);
+        }
+    }
+
+     public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+    }
+
+}
